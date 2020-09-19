74457e726c299da98d39732925a1b05aef1d527d
Erick Castille <ecastille924@gmail.com>
2020-09-19 23:11:19 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,2 @@
 def first_challenge
-   epic_tragedy = {
-     :montague => {
-       :partiarch => {},
-       :matriarch => {},
-       :hero => {},
-       :hero_friends => [ ]
-     }, 
-     :capulet => {
-       :patriarch => {},
-       :matriarch => {}, 
-       :heroine => {},
-       :heroine_friends => [ ]
-     }
-   }
 end
